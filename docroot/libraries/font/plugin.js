aea7ac09ed016cc75c79939e6c978a4a1eedbf48
Barry Lawson <blawson@www.colorado.gov>
2017-01-06 14:54:09 -0700

    Updated ckeditor font sizes

--- docroot/libraries/font/plugin.js
+++ docroot/libraries/font/plugin.js
@@ -318,7 +318,7 @@ CKEDITOR.config.font_style = {
  * @cfg {String} [fontSize_sizes=see source]
  * @member CKEDITOR.config
  */
-CKEDITOR.config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px';
+CKEDITOR.config.fontSize_sizes = '12/12px;14/14px;16/16px;18/18px;20/20px;24/24px;28/28px;36/36px;48/48px;64/64px;72/72px;84/84px';
 
 /**
  * The text to be displayed in the Font Size combo is none of the available
