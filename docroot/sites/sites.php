6b1e0b37d9c5b735e3bb55185fb44378718e027e
alne1147 <alne1147@live.com>
2017-02-22 12:41:19 -0700

    adding a symling to site1

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,6 +1,8 @@
 <?php
 
     $sites = array(
+      'coloradod8mdev.prod.acquia-sites.com.site1' => 'site1',
+      'coloradod8mdev.prod.acquia-sites.com.site1' => 'site1',
       'coloradod8mdev.prod.acquia-sites.com.site1.com' => 'site1',
       'site1.com.coloradod8mdev.prod.acquia-sites.com' => 'site1',
     );
