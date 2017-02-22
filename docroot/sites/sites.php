8c140f3dc7441ec3a689faf18fcfd4f27a2c2d98
alne1147 <alne1147@live.com>
2017-02-22 12:13:49 -0700

    adding .com in site url.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,7 +1,7 @@
 <?php
 
     $sites = array(
-      'coloradod8mdev.prod.acquia-sites.com.site1' => 'site1',
-      'site1.coloradod8mdev.prod.acquia-sites.com' => 'site1',
+      'coloradod8mdev.prod.acquia-sites.com.site1.com' => 'site1',
+      'site1.com.coloradod8mdev.prod.acquia-sites.com' => 'site1',
     );
 ?>
